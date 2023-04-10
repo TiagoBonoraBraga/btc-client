97343ed4aa302d3a9b6c383f32b2e2434df1a968
TiagoBraga <ogait.desenvolvedor.jr@gmail.com>
2023-04-10 18:59:01 -0300

    refactor: logo style

--- src/components/atoms/AtomsStyles/Logo.ts
+++ src/components/atoms/AtomsStyles/Logo.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 
 export const LogoBox = styled.div`
-position: absolute;
+/* position: absolute; */
     /* width: 200px; */
     height: 100%;
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    margin-bottom: 20px;
     /* padding-left: 40px; */
     h1{
         color: #2D9BF0;
