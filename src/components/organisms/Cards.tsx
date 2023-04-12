47c34a74c4d028b79b8a1f1094b934eaa966d96b
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-12 10:20:26 -0300

    feat: created cards-components

--- src/components/organisms/Cards.tsx
+++ src/components/organisms/Cards.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { CardContainer, CardDescription, CardImage, CardTitle } from './AtomsStyles/Card';
 
-const Cards = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface CardsProps {
+  title: string;
+  image: string;
+  description: string;
+  onClick?: () => void;
 }
+  
+const Cards: React.FC<CardsProps> = ({ title, image, description, onClick }) => {
+  return (
+    <>
+      <CardContainer onClick={onClick}>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+        {/* <CardImage src={image} alt="" /> */}
+      </CardContainer>
+    </>
+  );
+};
 
-export default Cards
+export default Cards;
