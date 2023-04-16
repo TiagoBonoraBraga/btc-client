9cef4fd34ffeecb41a28677b6d6c2c42b4aed3e9
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-15 22:26:22 -0300

    feat: implemente SuportPage.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import FormInput, { InputProps } from "../components/atoms/FormInput";
+import SubmitButton from "../components/atoms/SubmitButton";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import TextInput from "../components/atoms/TextInput";
+import Header from "../components/organisms/Header";
+import SubTitle from "../components/atoms/SubTitle";
+import {
+  SectionSupport,
+  StyleButton,
+  SupportContainer,
+} from "./StylePages/Support";
 
-const Support = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface SupportRequest {
+  name: string;
+  subject: string;
+  message: string;
 }
 
-export default Support
+const Support = () => {
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const supportPayload: SupportRequest = {
+      name: name,
+      subject: subject,
+      message: message,
+    };
+    console.log(supportPayload);
+    setName("");
+    setSubject("");
+    setMessage("");
+  }
+
+  function handleNameChange(event: ChangeEvent<HTMLTextAreaElement>): void {
+    setMessage(event.target.value);
+  }
+
+  return (
+    <>
+      <Header />
+      <SectionSupport>
+        <SubTitle titulo={"Solicitação de Suporte"} />
+        <SupportContainer as="form" onSubmit={handleSubmit}>
+          <FormInput
+            label={"Nome"}
+            placeholder={""}
+            value={name}
+            setValue={setName}
+            type={""}
+          />
+          <FormInput
+            label={"Assunto"}
+            placeholder={""}
+            value={subject}
+            setValue={setSubject}
+            type={""}
+          />
+          <TextInput
+            onChange={handleNameChange}
+            placeholder="Escreva aqui sua mensagem"
+            value={message}
+          />
+          <StyleButton>
+            <SubmitButton title={"Enviar"} />
+          </StyleButton>
+        </SupportContainer>
+      </SectionSupport>
+    </>
+  );
+};
+
+export default Support;
