f9f100a1d5cfc12fca61d1657acdad52b6bae2fc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-18 02:22:46 -0300

    refactor: refactor Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,39 @@
-import Header from '../components/organisms/Header'
-import React from 'react'
+import NavBar from "../components/molecules/NavBar";
+import Header from "../components/organisms/Header";
+import React from "react";
 
-const Table = ({dados}:any) => {
-  return(
+const Table = ({ dados }: any) => {
+  return (
     <>
-    teste
-    <table>
-      {dados.cabecalho.map((elemento:any)=>
-      <th>
-        {elemento}        
-      </th>
-      )}
-      {dados.dadosTable.map((elemento:any)=>
-      <td>
-        {elemento}       
-      </td>
-      )}      
-    </table>
-      
+      teste
+      <table>
+        {dados.cabecalho.map((elemento: any) => (
+          <th>{elemento}</th>
+        ))}
+        {dados.dadosTable.map((elemento: any) => (
+          <td>{elemento}</td>
+        ))}
+      </table>
     </>
-  )
-}
-
-
+  );
+};
 
 const Home = () => {
   let dadosDaApi = {
-    cabecalho: ["ID", 'Nome',"Score", "Tier", "Comissões"],
-    dadosTable: [[1,"Leonardo", '10', 1, "Não"], [2,"Leonardo", '10', 1, "Não"], [3,"Leonardo", '10', 1, "Não"] ]
-  }
+    cabecalho: ["ID", "Nome", "Score", "Tier", "Comissões"],
+    dadosTable: [
+      [1, "Leonardo", "10", 1, "Não"],
+      [2, "Leonardo", "10", 1, "Não"],
+      [3, "Leonardo", "10", 1, "Não"],
+    ],
+  };
 
   return (
-  <>
-    <header>
-      <Header/>      
-    </header>
-      <Table dados={dadosDaApi}/>
-  </>
-  )
-}
+    <>
+      <header><Header /> </header>
+      {/* <Table dados={dadosDaApi} /> */}
+    </>
+  );
+};
 
-export default Home
+export default Home;
