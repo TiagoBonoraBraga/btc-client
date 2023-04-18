8b125fde9388787dbf21765bfbe64d98843102ca
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-18 02:20:35 -0300

    refactor: molecules NavBar.tsx

--- src/components/molecules/NavBar.tsx
+++ src/components/molecules/NavBar.tsx
@@ -1,21 +1,52 @@
-import { NavBarBox } from './MoleculesStyle/NavBar'
-import React from 'react'
+import React, { useState } from "react";
+import menuItens from "../atoms/menuItensAdm.json";
+import { useLocation } from "react-router-dom";
+import {
+  HeaderStyled,
+  LinkStyle,
+  LogoStyled,
+  MenuContentStyled,
+  MenuIconStyled,
+  MenuStyled,
+} from "./MoleculesStyle/NavBar";
+import menublue from "../../assets/menublue.png";
+import logo from "../../assets/logo.png";
 
 const NavBar = () => {
-  return (
-    <>
-        
-    <NavBarBox>  
-      
-        <li>Receita</li>
-        <li>Clientes</li>
-        <li>Produtos</li>
-        <li>Suporte</li>
-        <li>Sair</li>
-            
-    </NavBarBox>
-    </>
-  )
-}
+  const [show, setShow] = useState<boolean>(false);
 
-export default NavBar
+  const toggleMenu = () => {
+    setShow(!show);
+  };
+
+  const location = useLocation();
+
+  return (
+    <HeaderStyled>
+      <LogoStyled>
+        <a href="#">
+          <img src={logo} alt="logo" width="150px" />
+        </a>
+      </LogoStyled>
+      <MenuContentStyled>
+        <MenuIconStyled src={menublue} width="40px" onClick={toggleMenu} />
+      </MenuContentStyled>
+
+      <MenuStyled show={show}>
+        {menuItens.map((item) => (
+          <li key={item.id}>
+            <LinkStyle
+              to={item.url}
+              show={show}
+              className={location.pathname === item.url ? "active" : ""}
+            >
+              {item.text}
+            </LinkStyle>
+          </li>
+        ))}
+      </MenuStyled>
+    </HeaderStyled>
+  );
+};
+
+export default NavBar;
