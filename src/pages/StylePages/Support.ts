73f66e31c864eeb4906638fe74c481f4d8fdee28
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-18 02:23:55 -0300

    refactor: refactor style page support

--- src/pages/StylePages/Support.ts
+++ src/pages/StylePages/Support.ts
@@ -4,12 +4,13 @@ export const SectionSupport = styled.section`
   display: flex;
   width: 60%;
   margin: auto;
-  margin-top: 100px;
+  margin-top: 150px;
   flex-direction: column;
   align-items: center;
   border: 1px solid #2d9bf0;
   border-radius: 20px;
   background-color: #f2f2f2;
+  box-shadow: 0 0 20px 10px rgba(128, 128, 128, 0.3);
 `;
 
 export const SupportContainer = styled.div`
