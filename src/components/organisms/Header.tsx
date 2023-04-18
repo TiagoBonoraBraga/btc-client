3391aa0ccbc56acc5e92eaf73039d5780948ec92
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-18 02:21:45 -0300

    refactor: refactor header.tsx

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,12 +1,13 @@
-import { HeaderStyled, LogoStyled, MenuContentStyled, MenuIconStyled, MenuStyled } from './OrganismsStyle/Header'
-import { memo, useState } from 'react';
+import { HeaderStyled } from "./OrganismsStyle/Header";
+import { memo, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-import logo from '../../assets/logo.png';
-import menublue from '../../assets/menublue.png';
+import logo from "../../assets/logo.png";
+import menublue from "../../assets/menublue.png";
+import menuItens from "../atoms/menuItensAdm.json";
+import NavBar from "../molecules/NavBar";
 
 // import { FiMenu } from 'react-icons/fi'
-
-
 
 // import Logo from '../atoms/Logo'
 // import NavBar from '../molecules/NavBar'
@@ -16,45 +17,17 @@ const Header = () => {
 
   const toggleMenu = () => {
     setShow(!show);
-  }
+  };
 
+  const location = useLocation();
 
   return (
     <>
-
-    <HeaderStyled>
-      <LogoStyled>
-        <a href='#'>
-          <img src={logo} alt="logo" width="150px"/>
-        </a>
-      </LogoStyled>
-
-      <MenuContentStyled>
-        <MenuIconStyled src={menublue} width="40px" onClick={toggleMenu}/>              
-      </MenuContentStyled>
-
-      <MenuStyled show={show}>
-        <li>
-          <a href="#">Receita</a>
-        </li>
-        <li>
-          <a href="/client">Clientes</a>
-        </li>
-        <li>
-          <a href="/products">Produtos</a>
-        </li>
-        <li>
-          <a href="/support">Suporte</a>
-        </li>
-        <li>
-          <a href="/login">Sair</a>
-        </li>
-      </MenuStyled>
-    </HeaderStyled>
-
-
+      <HeaderStyled>
+        <NavBar />
+      </HeaderStyled>
     </>
-  )
-}
+  );
+};
 
-export default memo(Header)
+export default memo(Header);
