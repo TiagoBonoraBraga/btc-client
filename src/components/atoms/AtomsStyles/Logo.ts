50e8309a796bae2742989d14bd1f400d92b149cc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-18 02:19:57 -0300

    feat: implement style AtomsLogo

--- src/components/atoms/AtomsStyles/Logo.ts
+++ src/components/atoms/AtomsStyles/Logo.ts
@@ -1,18 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import logoHyperlocal from "../../../assets/hyperlocal.jpg";
 
 export const LogoBox = styled.div`
-/* position: absolute; */
-    /* width: 200px; */
-    height: 100%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    margin-bottom: 20px;
-    /* padding-left: 40px; */
-    h1{
-        color: #2D9BF0;
-        font-size: 2rem;
-        font-weight: 500;
-    }
-    
+  /* position: absolute; */
+  /* width: 200px; */
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 20px;
+  /* padding-left: 40px; */
+  h1 {
+    color: #2d9bf0;
+    font-size: 2rem;
+    font-weight: 500;
+  }
+`;
+export const Image = styled.div`
+  opacity: 0.4;
+  border-radius: 50%;
+  box-shadow: 0 0 5px 10px rgba(45, 155, 240, 0.4);
+  width: 400px;
+  height: 400px;
+  background-image: url(${logoHyperlocal});
+  background-size: cover;
 `;
