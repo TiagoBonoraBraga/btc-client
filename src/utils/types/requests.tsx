2ce25d252de55401740ec4b376cce5ec44c89189
TiagoBraga <ogait.desenvolvedor.jr@gmail.com>
2023-04-20 15:28:35 -0300

    feat: refactor requests

--- src/utils/types/requests.tsx
+++ src/utils/types/requests.tsx
@@ -1,17 +1,39 @@
+//login
 export type LoginRequest = {
     email: string;
     password: string;
 };
 
+//crud
 export type CreateProductRequest = {
     name: string;
-    commission: string;
-    score: string;
+    description: string;   
+    score: number;
+    price: number;
 };
 
 export type UpdateProductRequest = {
-    id?: string;
+    id: string;
     name: string;
-    commission: string;
-    score: string;
+    description: string;
+    score: number;
+    price: number;
+}
+
+export type CreateClientRequest = {
+    id: string; //external id
+    name: string;
+    adress: string;
+    cep: string;
+    cpf: string;
+    fone: string;
+}
+
+export type CreateFranchisedRequest = {
+    id: string;
+    name: string;
+    adress: string;
+    cep: string;
+    cnpj: string;
+    fone: string;
 }
