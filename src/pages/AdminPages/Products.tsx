1cf4aa5419e549c9d46fb672e5ddb94dd4acb7aa
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-21 19:06:09 -0300

    refactor: refactor amd implement modalProduct

--- src/pages/AdminPages/Products.tsx
+++ src/pages/AdminPages/Products.tsx
@@ -1,29 +1,40 @@
 import React, { useState } from 'react'
-
 import { BoxTable } from '../StylePages/Franchised'
 import Header from '../../components/organisms/Header'
 import SubTitle from '../../components/atoms/SubTitle'
 import TableProducts from '../../components/molecules/TableProducts'
+import products from "../../Mocks/products.json"
+import { RowsProductsProps } from '../../components/atoms/RowsProducts';
+import ModalProducts from '../../components/organisms/ModalProducts';
 
-const franchised = [
-    { nome: "Produto 1",  descricao: "Produto HyperLocal", comissao: 20},
-    { nome: "Produto 2",  descricao: "Produto HyperLocal", comissao: 20},
-    { nome: "Produto 3",  descricao: "Produto HyperLocal", comissao: 20},
-   
-]
 const Products = () => {
 
-  const [rows, setRows] = useState(franchised);
+  const [rows, setRows] = useState(products);
+  const [selectedProduct, setSelectedProduct] = useState<RowsProductsProps | undefined>(undefined);
+
+  function handleSelectProduct(product: RowsProductsProps) {
+    setSelectedProduct(product);
+  }
+
   return (
     <>
-    <Header/>
-    <BoxTable>
-      <SubTitle titulo="Produtos"/>
-       <TableProducts data={rows}/>
-    </BoxTable>    
+      <Header/>
+      <BoxTable>
+        <SubTitle titulo="Produtos"/>
+        <TableProducts data={rows} handleSelectProduct={handleSelectProduct} />
+        {selectedProduct && (
+          <ModalProducts
+            show={true}
+            handleClose={() => setSelectedProduct(undefined)}
+            productSelect={selectedProduct} onSave={function (product: RowsProductsProps): void {
+              throw new Error('Function not implemented.')
+            } }          />
+        )}
+      </BoxTable>    
     </>
   )
 }
 
 export default Products
+
 
