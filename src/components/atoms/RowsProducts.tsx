72afd98fe6f38dd19f74abcce819e90527a267c6
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-21 19:00:15 -0300

    feat: implement ModalEdit

--- src/components/atoms/RowsProducts.tsx
+++ src/components/atoms/RowsProducts.tsx
@@ -1,28 +1,56 @@
-import { BoxButton } from './AtomsStyles/Rows';
-import { FiEdit } from 'react-icons/fi';
-import React from 'react'
+import { BoxButton } from "./AtomsStyles/Rows";
+import { FiEdit } from "react-icons/fi";
+import React, { useEffect, useState } from "react";
+import ModalProducts from "../../components/organisms/ModalProducts";
 
 export type RowsProductsProps = {
-    nome: string;
-    descricao: string;
-    comissao: number; 
-}
-const RowsProducts = ({nome, descricao, comissao}: RowsProductsProps ) => {
+  name: string;
+  description: string;
+  commission: string;
+};
+
+const RowsProducts = ({ name, description, commission }: RowsProductsProps) => {
+  const [showModalProducts, setShowModalProducts] = useState<boolean>(false);
+  const [productSelect, setProductSelect] = useState<RowsProductsProps>();
+
   function handleClick() {
-
+    setProductSelect({ name, description, commission });
+    setShowModalProducts(true);
   }
-  return (
-    <tr>
-        <td>{nome}</td>
-        <td>{descricao}</td>
-        <td>{comissao}</td>
-        <td>
-        <BoxButton onClick={handleClick}>
-          <FiEdit color="#2D9BF0" />
-        </BoxButton>
-      </td>            
-    </tr>
-  )
-}
+  function closeModal() {
+    setShowModalProducts(false);
+  }
 
-export default RowsProducts
+  useEffect(() => {
+    console.log(productSelect);
+  }, [productSelect]);
+  
+  return (
+    <>
+      <tr>
+        <td>{name}</td>
+        <td>{description}</td>
+        <td>{commission}</td>
+        <td>
+          <BoxButton onClick={handleClick}>
+            <FiEdit color="#2D9BF0" />
+          </BoxButton>
+        </td>
+      </tr>
+      <td>
+  {showModalProducts && (
+    <ModalProducts
+      show={showModalProducts}
+      handleClose={closeModal}
+      productSelect={productSelect}
+      onSave={function (product: RowsProductsProps): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  )}
+</td>
+    </>
+  );
+};
+
+export default RowsProducts;
