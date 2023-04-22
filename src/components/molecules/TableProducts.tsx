f6ab8453bb7a0d79a5a3727eb03c236ef51708c4
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-22 01:51:19 -0300

    chore: add new TableHeadCel and new property

--- src/components/molecules/TableProducts.tsx
+++ src/components/molecules/TableProducts.tsx
@@ -15,12 +15,13 @@ const TableProducts = ({data}: TableProductsProps) => {
         <thead>                
             <TableHeadCel titulo="Nome" />
             <TableHeadCel titulo="Descrição" />
-            <TableHeadCel titulo="Comissão" />
+            <TableHeadCel titulo="Score" />
+            <TableHeadCel titulo="Valor" />
             <TableHeadCel titulo="Editar" />
         </thead>
          <tbody>
             {data.map((row: RowsProductsProps, index: any) =>
-                <RowsProducts key={index}  name={row.name} description={row.description} commission={row.commission}/>
+                <RowsProducts key={index} name={row.name} description={row.description} score={`${row.score}%`} price={`R$ ${row.price}`}/>
             )}
         </tbody>
     </BoxTable>
