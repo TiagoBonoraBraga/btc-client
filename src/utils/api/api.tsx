ea3f4de937f0bae512b905a45c47ba8b7b7d61ac
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-24 18:56:47 -0300

    chore: implement updateProduct

--- src/utils/api/api.tsx
+++ src/utils/api/api.tsx
@@ -1,89 +1,98 @@
-import { CreateProductRequest, LoginRequest, UpdateProductRequest } from "../types/requests"
+import {
+  CreateProductRequest,
+  LoginRequest,
+  UpdateProductRequest,
+} from "../types/requests";
 
-import axios from "axios"
+import axios from "axios";
 
-axios.defaults.baseURL = "https://btc-server-production-3ecc.up.railway.app";
+axios.defaults.baseURL = 'https://btc-server-production-3ecc.up.railway.app';
 axios.defaults.headers.post["Content-Type"] = "applications/json";
 
 axios.interceptors.request.use(
-    function (config) {
-        const token = localStorage.getItem("token");
-        if (token) {
-            config.headers.Authorization = "Bearer " + token;
-        }
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
+  function (config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
     }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 export const api = {
-    login: async ({ email, password }: LoginRequest) => {
-        try {
-            const response = await axios.post('rota', { email, password });
-            // console.log(response.data.token);
-            localStorage.setItem('token', response.data.token);
-            return response.data;
-        } catch (error) {
-            console.error(error);
-        }
-    },
-
-    // CRUD PRODUTOS
-
-    getProducts: async () => {
-        try {
-            const response = await axios.get('/product');
-            return response.data;
-        } catch (error) {
-            alert(error);
-        }
-    },
-
-    getProductsById: async (id: string) => {
-        try {
-            const response = await axios.get('/product/${id}');
-            if (!response.data) {
-                throw new Error("product not found");
-            }
-            return response.data;
-        } catch (error) {
-            alert(error);
-        }
-    },
-
-    createProduct: async (ProductPayLoad: CreateProductRequest) => {
-        try {
-            const response = await axios.post('/product', ProductPayLoad);
-            console.log(response)
-            return (response.data);
-        } catch (error) {
-            alert(error)
-        }
-    },
-
-    updateProduct: async (payload: UpdateProductRequest) => {
-        try {
-            const response = await axios.patch('/product/' + payload.id, {
-                name: payload.name,
-                commission: payload.commission,
-                score: payload.score
-            });
-            console.log()
-            return response.data;
-        } catch (error) {
-            alert(error);
-        }
-    },
-
-    deleteProduct: async (payload: string) => {
-        try {
-            const response = await axios.delete(`/product/${payload}`);
-            return response.data;
-        } catch (error) {
-            alert(error);
-        }
+  login: async ({ email, password }: LoginRequest) => {
+    try {
+      const response = await axios.post("rota", { email, password });
+      // console.log(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
+  },
 
-}
+  // CRUD PRODUTOS
+
+  getProducts: async () => {
+    try {
+      const response = await axios.get("/product");
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  getProductsById: async (id: string) => {
+    try {
+      const response = await axios.get("/product/${id}");
+      if (!response.data) {
+        throw new Error("product not found");
+      }
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  createProduct: async (ProductPayLoad: CreateProductRequest) => {
+    try {
+      console.log(ProductPayLoad);
+      const response = await axios.post("/product", ProductPayLoad);
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  updateProduct: async (updatedProductPayload: UpdateProductRequest) => {
+    try {
+      const response = await axios.patch(
+        `/product/${updatedProductPayload.id}`,
+        {
+          id: updatedProductPayload.id,
+          name: updatedProductPayload.name,
+          description: updatedProductPayload.description, // corrigido aqui
+          commission: updatedProductPayload.commission,
+          score: updatedProductPayload.score,
+          price: updatedProductPayload.price,
+        }
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+
+  deleteProduct: async (payload: string) => {
+    try {
+      const response = await axios.delete(`/product/${payload}`);
+      return response.data;
+    } catch (error) {
+      alert(error);
+    }
+  },
+};
