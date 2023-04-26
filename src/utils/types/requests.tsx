32357e93687af1407bafe41b90d65f963b80c069
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-25 21:08:08 -0300

    refactor: refactor

--- src/utils/types/requests.tsx
+++ src/utils/types/requests.tsx
@@ -1,42 +1,41 @@
 //login
 export type LoginRequest = {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 };
 
 //crud
 export type CreateProductRequest = {
-    id?: number,
-    name: string;
-    description: string;
-    commission: number;   
-    score: number;
-    price: number;
+  name: string;
+  description: string;
+  commission: number;
+  score: number;
+  price: number;
 };
 
 export type UpdateProductRequest = {
-    id: number;
-    name: string;
-    description: string;
-    commission: number;
-    score: number;
-    price: number;
-}
+  id: number;
+  name: string;
+  description: string;
+  commission: number;
+  score: number;
+  price: number;
+};
 
 export type CreateClientRequest = {
-    id: string; //external id
-    name: string;
-    adress: string;
-    cep: string;
-    cpf: string;
-    fone: string;
-}
+  id: string; //external id
+  name: string;
+  adress: string;
+  cep: string;
+  cpf: string;
+  fone: string;
+};
 
 export type CreateFranchisedRequest = {
-    id: string;
-    name: string;
-    adress: string;
-    cep: string;
-    cnpj: string;
-    fone: string;
-}
+  id: string;
+  name: string;
+  adress: string;
+  cep: string;
+  cnpj: string;
+  fone: string;
+};
