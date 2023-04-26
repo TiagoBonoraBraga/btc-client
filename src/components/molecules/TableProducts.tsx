05966f6ef2ff06ba8aeed48ba6a8f593af3ae84e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-25 21:05:50 -0300

    refactor: refactor

--- src/components/molecules/TableProducts.tsx
+++ src/components/molecules/TableProducts.tsx
@@ -1,32 +1,40 @@
-import RowsProducts, { RowsProductsProps } from '../atoms/RowsProducts';
+import RowsProducts, { RowsProductsProps } from "../atoms/RowsProducts";
 
-import { BoxTable } from './MoleculesStyle/Table';
-import React from 'react'
-import TableHeadCel from '../atoms/TableHeadCel';
+import { BoxTable } from "./MoleculesStyle/Table";
+import React from "react";
+import TableHeadCel from "../atoms/TableHeadCel";
 
 export interface TableProductsProps {
-    data: RowsProductsProps[];
-    handleSelectProduct: (product: RowsProductsProps) => void;
-  }
-
-const TableProducts = ({data}: TableProductsProps) => {
-  return (
-    <BoxTable>
-        <thead>                
-            <TableHeadCel titulo="Nome" />
-            <TableHeadCel titulo="Descrição" />
-            <TableHeadCel titulo="Comissão" />
-            <TableHeadCel titulo="Score" />
-            <TableHeadCel titulo="Valor" />
-            <TableHeadCel titulo="Editar" />
-        </thead>
-         <tbody>
-            {data.map((row: RowsProductsProps, index: any) =>
-                <RowsProducts key={index} name={row.name} description={row.description} commission={`${row.commission}%`} score={`${row.score}%`} price={`R$ ${row.price}`} id={row.id}/>
-            )}
-        </tbody>
-    </BoxTable>
-  )
+  data: RowsProductsProps[];
+  handleSelectProduct: (product: RowsProductsProps) => void;
 }
 
-export default TableProducts
+const TableProducts = ({ data }: TableProductsProps) => {
+  return (
+    <BoxTable>
+      <thead>
+        <TableHeadCel titulo="Nome" />
+        <TableHeadCel titulo="Descrição" />
+        <TableHeadCel titulo="Comissão" />
+        <TableHeadCel titulo="Score" />
+        <TableHeadCel titulo="Valor" />
+        <TableHeadCel titulo="Editar" />
+      </thead>
+      <tbody>
+        {data.map((row: RowsProductsProps, index: any) => (
+          <RowsProducts
+            key={index}
+            name={row.name}
+            description={row.description}
+            commission={`${row.commission}`}
+            score={`${row.score}`}
+            price={`${row.price}`}
+            id={row.id}
+          />
+        ))}
+      </tbody>
+    </BoxTable>
+  );
+};
+
+export default TableProducts;
