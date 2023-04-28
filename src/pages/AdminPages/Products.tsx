2057ec3eda03806529f2b71e383d55f7d1c2a196
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-28 02:07:04 -0300

    chore: add cilcularProgress

--- src/pages/AdminPages/Products.tsx
+++ src/pages/AdminPages/Products.tsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
-
-import { BoxTable } from '../StylePages/Franchised'
-import Header from '../../components/organisms/Header'
-import ModalProducts from '../../components/organisms/ModalProducts';
-import { RowsProductsProps } from '../../components/atoms/RowsProducts';
-import SubTitle from '../../components/atoms/SubTitle'
-import TableProducts from '../../components/molecules/TableProducts'
-import { api } from '../../utils/api/api';
+import React, { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import { BoxTable } from "../StylePages/Franchised";
+import Header from "../../components/organisms/Header";
+import ModalProducts from "../../components/organisms/ModalProducts";
+import { RowsProductsProps } from "../../components/atoms/RowsProducts";
+import SubTitle from "../../components/atoms/SubTitle";
+import TableProducts from "../../components/molecules/TableProducts";
+import { api } from "../../utils/api/api";
 
 const Products = () => {
-
   const [rows, setRows] = useState<RowsProductsProps[]>([]);
-  const [selectedProduct, setSelectedProduct] = useState<RowsProductsProps | undefined>(undefined);
+  const [selectedProduct, setSelectedProduct] = useState<
+    RowsProductsProps | undefined
+  >(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const products = await api.getProducts();
       setRows(products);
+      setIsLoading(false);
     }
     fetchData();
   }, []);
@@ -27,23 +31,31 @@ const Products = () => {
 
   return (
     <>
-      <Header/>
+      <Header />
       <BoxTable>
-        <SubTitle titulo="Produtos"/>
-        <TableProducts data={rows} handleSelectProduct={handleSelectProduct} />
+        <SubTitle titulo="Produtos" />
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <TableProducts
+            data={rows}
+            handleSelectProduct={handleSelectProduct}
+          />
+        )}
         {selectedProduct && (
           <ModalProducts
             show={true}
             handleClose={() => setSelectedProduct(undefined)}
-            productSelect={selectedProduct} onSave={function (product: RowsProductsProps): void {
-              throw new Error('Function not implemented.')
-            } } id={"0"}          />
+            productSelect={selectedProduct}
+            onSave={function (product: RowsProductsProps): void {
+              throw new Error("Function not implemented.");
+            }}
+            id={"0"}
+          />
         )}
-      </BoxTable>    
+      </BoxTable>
     </>
-  )
-}
+  );
+};
 
-export default Products
-
-
+export default Products;
