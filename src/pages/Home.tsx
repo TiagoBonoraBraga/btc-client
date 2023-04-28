8064862671244ac77cf3acd9dcde996b7b9fa17c
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-28 18:48:48 -0300

    refactor: refactoring HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,11 @@
-import NavBar from "../components/molecules/NavBar";
-import Header from "../components/organisms/Header";
 import React from "react";
+import Header from "../components/organisms/Header";
 
-const Table = ({ dados }: any) => {
-  return (
-    <>
-      teste
-      <table>
-        {dados.cabecalho.map((elemento: any) => (
-          <th>{elemento}</th>
-        ))}
-        {dados.dadosTable.map((elemento: any) => (
-          <td>{elemento}</td>
-        ))}
-      </table>
-    </>
-  );
-};
 
 const Home = () => {
-  let dadosDaApi = {
-    cabecalho: ["ID", "Nome", "Score", "Tier", "Comissões"],
-    dadosTable: [
-      [1, "Leonardo", "10", 1, "Não"],
-      [2, "Leonardo", "10", 1, "Não"],
-      [3, "Leonardo", "10", 1, "Não"],
-    ],
-  };
-
   return (
     <>
-      <header><Header /> </header>
-      {/* <Table dados={dadosDaApi} /> */}
+      <Header />
     </>
   );
 };
