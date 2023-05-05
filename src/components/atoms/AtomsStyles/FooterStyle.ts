f76439ace247d7af40123b5ed0e7232b14298bcc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-05-04 21:33:10 -0300

    feat: implement Footerstyle.ts

--- src/components/atoms/AtomsStyles/FooterStyle.ts
+++ src/components/atoms/AtomsStyles/FooterStyle.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: aliceblue;
   padding: 1rem;
   text-align: center;
