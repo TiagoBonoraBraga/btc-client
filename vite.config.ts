f9b3caacc63341fc5142d223c4aa8591a972b990
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-05-04 21:31:25 -0300

    feat: add tailwindPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
+const tailwindPlugin = tailwindcss({
+  config: './tailwind.config.js',
+}) ;
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    
+  ],
+});
+
+
+
