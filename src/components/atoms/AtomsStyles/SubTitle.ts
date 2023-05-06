85da1c8bfbce0803bf99c0253fd64d32a07138e2
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-05-06 18:18:28 -0300

    fix: responsivity setup

--- src/components/atoms/AtomsStyles/SubTitle.ts
+++ src/components/atoms/AtomsStyles/SubTitle.ts
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 export const SubTitleForm = styled.h2`
-    color: #2D9BF0;
+    color: #2F3178;
     font-size: 35px;
     font-weight: 500;
     margin-top: 1rem;
     margin-bottom: .5rem;
+    
+    @media (max-width: 768px) {
+        text-align: center;
+        padding: 0.5em;
+        font-size: 1.5em;
+        align-items: center;
+    }
    
 `;
