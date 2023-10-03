382ab65d1b11c757b159648d97a767e25021199d
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-10-03 13:35:17 -0300

    style: changed height

--- src/pages/StylePages/Franchised.ts
+++ src/pages/StylePages/Franchised.ts
@@ -6,11 +6,10 @@ export const BoxTable = styled.section`
     align-items: center;
     justify-content: flex-start;
     max-width: 1000px;
-    height: 450px;
+    height: auto;
     margin: 0 auto;
     margin-top: 100px;
     border: 1px solid #2D9BF0;
     border-radius: 10px;
-    
     
 `;
